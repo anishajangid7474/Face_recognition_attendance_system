28f23641549eb875e4979e2df60b820f6d67e1fd
Anisha Jangid <118589918+anishajangid7474@users.noreply.github.com>
2023-06-06 18:24:15 +0530

    Add files via upload

---
+++ AttendanceProject.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+import pyttsx3 as voice
+from datetime import datetime
+
+speech=voice.init()
+
+rate = speech.getProperty('rate')
+speech.setProperty('rate', 150)
+
+# from PIL import ImageGrab
+
+path = 'ImagesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString =now.strftime('%d/%m/%y,%I:%M %p')
+            f.writelines(f'\n{name},{dtString}')
+            statement = str('Welcome to Class' + name)
+            speech.say(statement)
+            speech.runAndWait()
+
+#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
+# def captureScreen(bbox=(300,300,690+300,530+300)):
+#     capScr = np.array(ImageGrab.grab(bbox))
+#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+#     return capScr
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+# img = captureScreen()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+# print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if faceDis[matchIndex] < 0.50:
+            name = classNames[matchIndex].upper()
+            markAttendance(name)
+        else:
+            name = 'Unknown'
+            speech.say('Please next')
+            speech.runAndWait()
+        # print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 300, 0), 2)
+        cv2.rectangle(img, (x1, y2), (x2, y2), (255, 300, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        #markAttendance(name)
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
